f082b9754c20c8c14595145cb16b4b941922c607
Lucas Silva <lucas@brainyresolution.com>
2021-07-17 08:17:55 -0300

    chore: added global styles

---
+++ src/utils/globalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+import GothamBookWoff2 from "../assets/fonts/Gotham-Book.woff2";
+import GothamBoldWoff2 from "../assets/fonts/Gotham-Bold.woff2";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: 'Gotham Book';
+    margin: 0;
+    padding: 0;
+}
+
+html, body
+{
+    height: "100%";
+}
+`;
+
+export const FontStyles = createGlobalStyle`
+
+@font-face {
+font-family: 'Gotham Book';
+src: url(${GothamBookWoff2}) format('woff2'),
+}
+
+@font-face {
+font-family: 'Gotham Bold';
+src: url(${GothamBoldWoff2}) format('woff2');
+font-weight: bold;
+}
+`;
---
+++ src/utils/style.js
@@ -0,0 +1,17 @@
+export const color = {
+  textDarkest: "#3E4157",
+  textDark: "#9196AB",
+
+  backgroundDarkPrimary: "#0747A6",
+  backgroundMedium: "#EBEEF6",
+  backgroundLight: "#FAFBFF",
+  backgroundLightest: "#FFFFFF",
+
+  borderLightest: "#CCCFDE",
+};
+
+export const font = {
+  regular: 'font-family: "Gotham Book"; font-weight: normal;',
+  bold: 'font-family: "Gotham Bold"; font-weight: bold;',
+  size: (size) => `font-size: ${size}px;`,
+};
