55815837e66c9776ea7e2cd297da395d16e49022
Lucas Silva <lucas@brainyresolution.com>
2021-07-17 14:55:18 -0300

    feature: created divider for navbar

---
+++ src/components/divider.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Divider = styled.div`
+  width: 21px;
+  height: 3px;
+  background-image: linear-gradient(to right, #00e1ff, #117eff);
+`;
+
+export default Divider;
